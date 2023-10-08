8cecf40d329459df0987f9f59c061c997959b75d
Viane Matsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-10-08 16:08:34 -0400

    Add files via upload

---
+++ juke-sample-rest-service/src/main/java/com/example/GreetingController.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.juke.framework.api.JukeFactory;
+import org.juke.framework.api.JukeState;
+
+import javax.annotation.PostConstruct;
+
+
+/**
+ *
+ *
+ */
+@RestController
+public class GreetingController {
+
+	
+
+@Autowired
+private IGreetingsService service;
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return service.greeting(name);
+
+	}
+	@PostConstruct
+	public void initialized() {
+		
+		this.service= (IGreetingsService) new JukeFactory<IGreetingsService>().newInstance(this.service,
+			IGreetingsService.class, JukeState.juke);
+		System.out.println("initialized");
+	}
+}
---
+++ juke-sample-rest-service/src/main/java/com/example/GreetingServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingServiceImpl implements IGreetingsService {
+	private static final String template = "Hello, %s!";
+
+	private final AtomicLong counter = new AtomicLong();
+	@Override
+	public Greeting greeting(String name) {
+		// TODO Auto-generated method stub
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+}
